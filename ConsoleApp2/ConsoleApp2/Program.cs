35deed4e9d433213ba0c051fecea733ef83fcfde
Brian Henderson <henderson.brian82@gmail.com>
2020-08-25 14:47:07 -0400

    Added Switch Statement Exercise

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite subject to study?");
+            var subject = (Console.ReadLine());
+
+            switch (subject)
+            {
+                case "Math":
+                    Console.WriteLine("Oh, so you're a numbers person, cool!");
+                    break;
+                case "History":
+                    Console.WriteLine("If we don't learn from it, we're doomed to repeat it.");
+                    break;
+                case "English":
+                    Console.WriteLine("Maybe you're a bibliophile.");
+                    break;
+                case "Science":
+                    Console.WriteLine("I wonder which particular science you like...");
+                    break;
+                case "Foreign Languages":
+                    Console.WriteLine("Kinda sucks that we can't travel much of anywhere right now.");
+                    break;
+                case "Music":
+                    Console.WriteLine("Awesome!  I like that, too.  If it sounds good, it IS good!");
+                    break;
+                default:
+                    Console.WriteLine("I've never studied that.  But you do you.");
+                    break;
+            }
+        }
+    }
+}
